1b615eb0cb083070865fb3e888e7266ba2eeac3d
Shubham <shubhamyadav97660@gmail.com>
2025-02-24 23:37:03 +0530

    working with backend, application

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -2,18 +2,7 @@ const express = require("express");
 const Job = require("../models/Job");
 const router = express.Router();
 const mongoose = require("mongoose");
-// // Create Job
-// router.post("/", async (req, res) => {
-//   try {
-//     const newJob = new Job(req.body);
-//     await newJob.save();
-//     res.json({ message: "Job posted successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
-// Create Job
 router.post("/", async (req, res) => {
   try {
     const { title, salary, workExperience, typeOfEmployment, description, jobImage, location, postedBy} = req.body;
@@ -58,27 +47,6 @@ router.post("/bulk", async (req, res) => {
   }
 })
 
-// router.post("/bulk", async (req, res) => {
-//   try {
-//     const jobs = req.body; // Expecting an array of job objects
-
-//     // Validate each job's `postedBy` as ObjectId
-//     const validJobs = jobs.map(job => ({
-//       ...job,
-//       postedBy: new mongoose.Types.ObjectId(job.postedBy)
-//     }));
-
-//     // Insert jobs into database
-//     const insertedJobs = await Job.insertMany(validJobs);
-    
-//     res.status(201).json({ message: "Jobs posted successfully!", insertedJobs });
-
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// Get All Jobs
 router.get("/", async (req, res) => {
   try {
     const jobs = await Job.find().populate("postedBy", "company");
@@ -89,53 +57,15 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Get All Jobs
-// router.get("/", async (req, res) => {
-//   try {
-//     const jobs = await Job.find().populate("postedBy", "name companyDetails");
-//     if (jobs.length === 0) {
-//       return res.json({ message: "No jobs found" });
-//     }
-//     res.json(jobs);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
+router.get("/:id", async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const job = await Job.find({_id: req.params.id});
+    console.log(job);
+    res.json(job)
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+})
 
 module.exports = router;
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const Job = require("../models/Job");
-
-// const router = express.Router();
-
-// // Create Job
-// router.post("/", async (req, res) => {
-//   try {
-//     const { title, company, location, salary, description, requirements, postedBy } = req.body;
-
-//     // Convert postedBy (string) to ObjectId
-//     if (!mongoose.Types.ObjectId.isValid(postedBy)) {
-//       return res.status(400).json({ error: "Invalid employer ID" });
-//     }
-
-//     const newJob = new Job({
-//       title,
-//       company,
-//       location,
-//       salary,
-//       description,
-//       requirements,
-//       postedBy: new mongoose.Types.ObjectId(postedBy)  // Convert string to ObjectId
-//     });
-
-//     await newJob.save();
-//     res.json({ message: "Job posted successfully!" });
-
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// module.exports = router;
